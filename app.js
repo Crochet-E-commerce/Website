2ca46d867df74d78db4a1d866e6711a6741e608c
RachelX <xia.rong.rachel@gmail.com>
2024-03-19 15:54:44 -0700

    sql created

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
 import express from 'express';
+import pg from "pg";
+import categories from "./data/categories.json" assert { type: 'json' };
 
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 
-const item = [
-    {name:'Flower Blanket', details:'Discover our cozy flower blanket, handcrafted with soft yarn in beautiful colors.', size:'15 in * 15 in',yarn:'xxxx', category:'other', price:'150', img:'0', type: 'trending'},
-    {name:'Lemon', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'20',img:'1', type: 'trending'},
-    {name:'Strawberry', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'25', img:'2', type: 'trending'},
-    {name:'Mashroom Cardigan', details:'Discover our cozy flower blanket, handcrafted with soft yarn in beautiful colors.', size:'15 in * 15 in',yarn:'xxxx', category:'other', price:'150', img:'0', type: 'trending'},
-    {name:'Rabbit', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'20',img:'1', type: 'popular'},
-    {name:'Octopus', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'25', img:'2',type: 'popular'},
-    {name:'Tulip', details:'Discover our cozy flower blanket, handcrafted with soft yarn in beautiful colors.', size:'15 in * 15 in',yarn:'xxxx', category:'other', price:'150', img:'0',type: 'popular'},
-    {name:'Patch Top', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'20',img:'1', type: 'popular'},
-    {name:'Turtles', details:'Handcrafted with soft yarn in beautiful colors. Looks like real lemon.', size:'10 in * 6 in',yarn:'ttxx', category:'fruit',  price:'25', img:'2'},
-];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('images'));
 
-const categories = [
-    {name:'All',img:'0'},
-    {name:'Animal', img:'1'},
-    {name:'Flower', img:'2'},
-    {name:'Fruit', img:'3'},
-    {name:'Character', img:'4'},
-    {name:'Cardigan', img:'5'},
-];
+//middleware
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "items",
+    password: "0108",
+    port: 5432,
+  });
+
+  db.connect()
+    .then(() => console.log('Database connected'))
+    .catch(err => console.error('Error connecting to database', err));
+
+  app.use(async (req, res, next) => {
+    try {
+        const result = await db.query('SELECT * FROM products');
+        req.products = result.rows;
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+//
 
 app.get('/', (req, res) => {
     var tagline = "Discover the finest crochet products"
     res.render('pages/index',{
         categories:categories,
         tagline:tagline,
-        item:item
+        item:req.products
     })
 })
 
 app.get('/items/:name', (req, res) => {
     const itemName = req.params.name;
-    const selectedItem = item.find(item => item.name === itemName)
+    const selectedItem = req.products.find(item => item.name === itemName)
     res.render('pages/items', { item: selectedItem });
 });
 
@@ -46,7 +54,7 @@ app.get('/shop/:id', (req, res) => {
     const selectedShop = categories[shopId];
     res.render('pages/shop', { 
         shop: selectedShop,
-        item:item
+        item:req.products
      });
 });
 
---
+++ crochet_items_dump.sql
@@ -0,0 +1,1 @@
+pg_dump -U postgres -d items -f database_dump.sql
